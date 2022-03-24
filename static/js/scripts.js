53339b2b33b3a4176811e984ede94a62e995cc50
Chiranshu Walia <waliachiranshu77@gmail.com>
2022-03-24 18:54:14 +0530

    menu icon fixed

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -23,16 +23,16 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     function _toggleMenuIcon() {
-        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
-        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-times');
-        if (menuToggleBars) {
-            menuToggleBars.classList.remove('fa-bars');
-            menuToggleBars.classList.add('fa-times');
-        }
-        if (menuToggleTimes) {
-            menuToggleTimes.classList.remove('fa-times');
-            menuToggleTimes.classList.add('fa-bars');
-        }
+        // const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
+        // const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-times');
+        // if (menuToggleBars) {
+        //     menuToggleBars.classList.remove('fa-bars');
+        //     menuToggleBars.classList.add('fa-times');
+        // }
+        // if (menuToggleTimes) {
+        //     menuToggleTimes.classList.remove('fa-times');
+        //     menuToggleTimes.classList.add('fa-bars');
+        // }
     }
 
     // Scroll to top button appear
